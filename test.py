0ebcb6a60d5477b127db0b497224e42077ec1bd8
hectorstewart20 <hector.stewart20@gmail.com>
2020-05-29 00:29:59 -0500

    primer commit

---
+++ test.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import unittest
+import time
+
+tc = unittest.TestCase('__init__')
+driver = webdriver.Chrome('chromedriver.exe')
+driver.get('http://automationpractice.com/index.php')
+driver.find_element_by_id('search_query_top').send_keys('hola')
+driver.find_element_by_name('submit_search').click()
+msg_error = driver.find_element_by_xpath('//*[@id="center_column"]/p')
+time.sleep(2)
+tc.assertEqual('No results were found for your search "hola"',msg_error.text)
+driver.close()
+driver.quit()
+
